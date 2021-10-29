cb20a2968c43e08db37edc5331851c3f41d77b13
ArtisanSelects <85949213+ArtisanSelects@users.noreply.github.com>
2021-10-29 16:04:34 -0400

    Added header check middleware

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.use(helmet());
 app.use(compression());
 app.use(express.json());
 
+app.use((req, res, next) => {
+    if (req.headers['X-RapidAPI-Proxy-Secret'] !== process.env.ApiProxySecret) {
+        res.status(401);
+    } else {
+        next();
+    }
+});
+
 app.use("/:palindrome", APIController.palindrome_get);
 app.use("*", (req, res) => res.status(400).json({ success: false, response: 'ERROR: No palindrome supplied.' }));
 
